7308c32d6648896170cb853f884ab0b6e6dafc12
abdullah <44318011+fx2y@users.noreply.github.com>
2020-05-31 07:10:56 +0700

    sync

--- shared/lib/devbox.sh
+++ shared/lib/devbox.sh
@@ -2,11 +2,11 @@
 
 # source: https://docs.conda.io/en/latest/miniconda.html
 shared_setup_conda() {
-  CONDA_OS=${1:-"Linux"} # Or, MacOSX
-  wget -qO /tmp/conda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-"$CONDA_OS"-x86_64.sh
-  (cd /tmp && bash conda.sh -b && rm conda.sh && ~/miniconda3/bin/conda init)
-  export PATH="$HOME/miniconda3/bin:$PATH"
-  cat <<EOF >>~/.condarc
+	CONDA_OS=${1:-"Linux"} # Or, MacOSX
+	wget -qO /tmp/conda.sh https://repo.anaconda.com/miniconda/Miniconda3-latest-"$CONDA_OS"-x86_64.sh
+	(cd /tmp && bash conda.sh -b && rm conda.sh && ~/miniconda3/bin/conda init)
+	export PATH="$HOME/miniconda3/bin:$PATH"
+	cat <<EOF >>~/.condarc
 channels:
   - pytorch
   - defaults
@@ -16,50 +16,72 @@ EOF
 
 # source: https://rustup.rs
 shared_setup_rustup() {
-  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path --default-toolchain beta --quiet -y
-  echo "export PATH=\"\$HOME/.cargo/bin:\$PATH\"" >>~/.bash_profile.local
-  export PATH="$HOME/.cargo/bin:$PATH"
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- --no-modify-path --default-toolchain beta --quiet -y
+	echo "export PATH=\"\$HOME/.cargo/bin:\$PATH\"" >>~/.bash_profile.local
+	export PATH="$HOME/.cargo/bin:$PATH"
 }
 
 # source: https://asdf-vm.com/#/core-manage-asdf-vm
 shared_setup_asdf() {
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
-  echo ". \"\$HOME/.asdf/asdf.sh\"" >>~/.bash_profile.local
-  echo ". \"\$HOME/.asdf/completions/asdf.bash\"" >>~/.bash_profile.local
-  # shellcheck source=$HOME/.asdf/asdf.sh
-  . "$HOME/.asdf/asdf.sh"
+	git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.8
+	echo ". \"\$HOME/.asdf/asdf.sh\"" >>~/.bash_profile.local
+	echo ". \"\$HOME/.asdf/completions/asdf.bash\"" >>~/.bash_profile.local
+	# shellcheck source=$HOME/.asdf/asdf.sh
+	. "$HOME/.asdf/asdf.sh"
 }
 
 shared_setup_nodejs() {
-  util_setup_asdf nodejs "bash $HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring"
+	util_setup_asdf nodejs "bash $HOME/.asdf/plugins/nodejs/bin/import-release-team-keyring"
 }
 
 shared_setup_golang() {
-  util_setup_asdf golang
+	util_setup_asdf golang
 }
 
 shared_setup_elixir() {
-  util_setup_asdf erlang
-  util_setup_asdf elixir
+	util_setup_asdf erlang
+	util_setup_asdf elixir
 }
 
 util_setup_asdf() {
-  asdf plugin add "$1"
-  ${2}
-  util_update_asdf "$1"
+	asdf plugin add "$1"
+	${2}
+	util_update_asdf "$1"
 }
 
 util_update_asdf() {
-  asdf list all "$1"
-  echo "Read New $1 Version:"
-  read -r ASDF_VERSION
-  asdf install "$1" "$ASDF_VERSION"
-  asdf global "$1" "$ASDF_VERSION"
+	asdf list all "$1"
+	echo "Read New $1 Version:"
+	read -r ASDF_VERSION
+	asdf install "$1" "$ASDF_VERSION"
+	asdf global "$1" "$ASDF_VERSION"
 }
 
 util_delete_asdf() {
-  asdf list "$1"
-  echo "Read Old $1 Version:"
-  read -r ASDF_VERSION
-  asdf uninstall "$1" "$ASDF_VERSION"
+	asdf list "$1"
+	echo "Read Old $1 Version:"
+	read -r ASDF_VERSION
+	asdf uninstall "$1" "$ASDF_VERSION"
+}
+
+shared_setup_lsp() {
+	lsp_python
+	lsp_rust
+}
+
+lsp_python() {
+	pip install -U setuptools
+	pip install 'python-language-server[all]'
+	pip install pyls-mypy pyls-isort pyls-black
+	pip install ptvsd
+}
+
+lsp_rust() {
+	mkdir -p ~/.local/bin
+	curl -L https://github.com/rust-analyzer/rust-analyzer/releases/latest/download/rust-analyzer-linux -o ~/.local/bin/rust-analyzer
+	chmod +x ~/.local/bin/rust-analyzer
+	sudo apt install -y lldb-10 liblldb-10 liblldb-10-dev
+	mkdir -p ~/.vscode/extensions/llvm-org.lldb-vscode-0.1.0/bin
+	cp package.json ~/.vscode/extensions/llvm-org.lldb-vscode-0.1.0
+	(cd ~/.vscode/extensions/llvm-org.lldb-vscode-0.1.0/bin && cp /usr/lib/llvm-10/lib/liblldb.so . && cp /usr/lib/llvm-10/bin/lldb-vscode .)
 }
--- ubuntu/lib/devbox.sh
+++ ubuntu/lib/devbox.sh
@@ -3,62 +3,77 @@
 . ../shared/lib/devbox.sh
 
 setup_devbox() {
-  setup_conda
-  shared_setup_rustup
-  setup_asdf
-  shared_setup_nodejs
-  shared_setup_golang
-  setup_elixir
-  setup_jetbrains
+	setup_conda
+	shared_setup_rustup
+	setup_asdf
+	shared_setup_nodejs
+	shared_setup_golang
+	setup_elixir
+	setup_jetbrains
 }
 
 # source: https://docs.conda.io/en/latest/miniconda.html
 setup_conda() {
-  shared_setup_conda
-  conda install -y pytorch torchvision cudatoolkit=10.2 -c pytorch
+	shared_setup_conda
+	conda install -y pytorch torchvision cudatoolkit=10.2 -c pytorch
 }
 
 setup_asdf() {
-  sudo apt install -y \
-    automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev \
-    libxslt-dev libffi-dev libtool unixodbc-dev unzip curl
-  shared_setup_asdf
+	sudo apt install -y \
+		automake autoconf libreadline-dev libncurses-dev libssl-dev libyaml-dev \
+		libxslt-dev libffi-dev libtool unixodbc-dev unzip curl
+	shared_setup_asdf
 }
 
 # source: https://github.com/asdf-vm/asdf-erlang
 # source: https://github.com/asdf-vm/asdf-elixir
 setup_elixir() {
-  sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6856E1DB1AC82609
-  sudo apt-add-repository 'deb https://repos.codelite.org/wx3.1.3/ubuntu/ eoan universe'
-  sudo apt update -y
-  sudo apt install -y \
-    build-essential autoconf m4 libncurses5-dev \
-    libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev \
-    xsltproc fop libwxbase3.1-0-unofficial3 \
-    libwxbase3.1unofficial3-dev \
-    libwxgtk3.1-0-unofficial3 \
-    libwxgtk3.1unofficial3-dev \
-    wx3.1-headers \
-    wx-common
-  export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac"
-  shared_setup_elixir
+	sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 6856E1DB1AC82609
+	sudo apt-add-repository 'deb https://repos.codelite.org/wx3.1.3/ubuntu/ eoan universe'
+	sudo apt update -y
+	sudo apt install -y \
+		build-essential autoconf m4 libncurses5-dev \
+		libgl1-mesa-dev libglu1-mesa-dev libpng-dev libssh-dev unixodbc-dev \
+		xsltproc fop libwxbase3.1-0-unofficial3 \
+		libwxbase3.1unofficial3-dev \
+		libwxgtk3.1-0-unofficial3 \
+		libwxgtk3.1unofficial3-dev \
+		wx3.1-headers \
+		wx-common
+	export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac"
+	shared_setup_elixir
 }
 
 update_elixir() {
-  export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac"
-  util_update_asdf erlang
-  util_update_asdf elixir
-  util_delete_asdf erlang
-  util_delete_asdf elixir
+	export KERL_CONFIGURE_OPTIONS="--disable-debug --without-javac"
+	util_update_asdf erlang
+	util_update_asdf elixir
+	util_delete_asdf erlang
+	util_delete_asdf elixir
 }
 
 setup_jetbrains() {
-  JETBRAINS_VERSION=1.17.6856
-  (cd /tmp && wget -qO jetbrains-toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-"$JETBRAINS_VERSION".tar.gz && tar -xvzf jetbrains-toolbox.tar.gz && rm jetbrains-toolbox.tar.gz)
-  mkdir -p ~/opt/bin
-  (cd /tmp && mv jetbrains-toolbox-"$JETBRAINS_VERSION"/jetbrain-toolbox ~/opt/bin && rm -rf jetbrains-toolbox-"$JETBRAINS_VERSION")
-  echo "export PATH=\"\$HOME/opt/bin:\$PATH\"" >>~/.bash_profile.local
-  export PATH="$HOME/opt/bin:$PATH"
-  jetbrains-toolbox
-  read -r
+	JETBRAINS_VERSION=1.17.6856
+	(cd /tmp && wget -qO jetbrains-toolbox.tar.gz https://download.jetbrains.com/toolbox/jetbrains-toolbox-"$JETBRAINS_VERSION".tar.gz && tar -xvzf jetbrains-toolbox.tar.gz && rm jetbrains-toolbox.tar.gz)
+	mkdir -p ~/opt/bin
+	(cd /tmp && mv jetbrains-toolbox-"$JETBRAINS_VERSION"/jetbrain-toolbox ~/opt/bin && rm -rf jetbrains-toolbox-"$JETBRAINS_VERSION")
+	echo "export PATH=\"\$HOME/opt/bin:\$PATH\"" >>~/.bash_profile.local
+	export PATH="$HOME/opt/bin:$PATH"
+	jetbrains-toolbox
+	read -r
+}
+
+setup_vscode() {
+	curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor >packages.microsoft.gpg
+	sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+	rm packages.microsoft.gpg
+	sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+	sudo apt-get install -y apt-transport-https
+	sudo apt-get update
+	sudo apt-get install -y code
+	echo "alias code='code --disable-font-subpixel-positioning'" >>~/.bash_profile.local
+	cat <<EOF
+  # sudo vi /usr/share/applications/code.desktop
+  # Exec=code --disable-font-subpixel-positioning
+EOF
 }
