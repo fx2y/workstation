2a5437359626454bce24e296e3e0199902502476
abdullah <44318011+fx2y@users.noreply.github.com>
2020-06-06 08:04:49 +0700

    sync

--- ubuntu/lib/devbox.sh
+++ ubuntu/lib/devbox.sh
@@ -16,6 +16,7 @@ setup_devbox() {
 setup_conda() {
 	shared_setup_conda
 	conda install -y pytorch torchvision cudatoolkit=10.2 -c pytorch
+	conda install -y dglteam cudatoolkit=10.2 -c dglteam
 }
 
 setup_asdf() {
