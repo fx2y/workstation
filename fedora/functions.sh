7a51dd6313f4dc74989de36047dfa86c4f15a52e
abdullah <44318011+fx2y@users.noreply.github.com>
2020-06-13 20:04:10 +0700

    sync

--- fedora/functions.sh
+++ fedora/functions.sh
@@ -3,40 +3,41 @@
 . ./lib/desktop.sh
 
 # https://rpmfusion.org/Configuration
-setup_rpmfusion() {
-  sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+setup_dnf() {
+	sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	sudo dnf config-manager --set-enabled rpmfusion-free-updates-testing rpmfusion-nonfree-updates-testing updates-testing updates-testing-modular
+	sudo dnf update -y
 }
 
 # https://rpmfusion.org/Howto/NVIDIA
 setup_nvidia() {
-  sudo dnf update -y
-  sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda  
+	sudo dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
 }
 
 # SHELL
 
 # https://dreameh.dev/blog/emacs-fedora-31
 setup_emacs() {
-  mkdir -p ~/git
-  sudo dnf builddep -y emacs &
-  (cd ~/git && git clone https://github.com/emacs-mirror/emacs.git --branch emacs-27) &
-  wait
-  update_emacs
+	mkdir -p ~/git
+	sudo dnf builddep -y emacs &
+	(cd ~/git && git clone https://github.com/emacs-mirror/emacs.git --branch emacs-27) &
+	wait
 }
 
 update_emacs() {
-  (cd ~/git/emacs && git pull --rebase && ./autogen.sh && ./configure && make && sudo make install)
+	(cd ~/git/emacs && git pull --rebase && ./autogen.sh && ./configure && make && sudo make install)
 }
 
 # https://github.com/hlissner/doom-emacs/blob/develop/docs/getting_started.org#fedora
 setup_doom_emacs() {
-  setup_emacs &
-  sudo dnf install -y git ripgrep tar fd-find clang multimarkdown ShellCheck &
-  git clone https://github.com/hlissner/doom-emacs ~/.emacs.d --branch develop &
-  wait
-  ~/.emacs.d/bin/doom install
+	setup_emacs
+	update_emacs &
+	sudo dnf install -y git ripgrep tar fd-find clang multimarkdown ShellCheck &
+	git clone https://github.com/hlissner/doom-emacs ~/.emacs.d --branch develop &
+	wait
+	~/.emacs.d/bin/doom install
 }
 
 update_doom_emacs() {
-  ~/.emacs.d/bin/doom upgrade
+	~/.emacs.d/bin/doom upgrade
 }
--- fedora/lib/desktop.sh
+++ fedora/lib/desktop.sh
@@ -1,5 +1,29 @@
 #!/usr/bin/env bash
 
 setup_desktop() {
-  sudo dnf install -y gnome-tweaks
+	sudo dnf install -y gnome-tweaks
+}
+
+setup_firefox() {
+	echo "Read Firefox Profile Directory (about:profiles):"
+	read -r FF_DIR
+	(
+		cd "$FF_DIR" || exit
+		curl -sSLo updater.sh https://raw.githubusercontent.com/ghacksuserjs/ghacks-user.js/master/updater.sh
+		touch user-overrides.js
+		bash updater.sh
+	)
+	echo "Further Hardening: https://github.com/ghacksuserjs/ghacks-user.js/wiki#small_orange_diamond-further-hardening"
+	echo "Install Bitwarden: https://addons.mozilla.org/en-US/firefox/addon/bitwarden-password-manager/"
+	echo "Install Hide Top Bar: https://extensions.gnome.org/extension/545/hide-top-bar/"
+	read -r
+}
+
+setup_gsettings() {
+	# Night Light
+	gsettings set get org.gnome.settings-daemon.plugins.color night-light-enabled true
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-automatic false
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-from 0.0
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-schedule-to 23.99
+	gsettings set org.gnome.settings-daemon.plugins.color night-light-temperature 4700
 }
